f091de49304b9e2733c51a8f19faf974400a6cff
javitocor <javitocor@gmail.com>
2020-05-14 15:56:44 +0200

    Modified models and added friendlyid

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200514104901) do
+ActiveRecord::Schema.define(version: 20200514135242) do
 
   create_table "channels", force: :cascade do |t|
     t.string "channel"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "discussion_id"
+    t.string "slug"
   end
 
   create_table "discussions", force: :cascade do |t|
@@ -26,6 +27,18 @@ ActiveRecord::Schema.define(version: 20200514104901) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.integer "channel_id"
+    t.string "slug"
+  end
+
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string "slug", null: false
+    t.integer "sluggable_id", null: false
+    t.string "sluggable_type", limit: 50
+    t.string "scope"
+    t.datetime "created_at"
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+    t.index ["sluggable_type", "sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id"
   end
 
   create_table "replies", force: :cascade do |t|
@@ -34,6 +47,7 @@ ActiveRecord::Schema.define(version: 20200514104901) do
     t.datetime "updated_at", null: false
     t.integer "discussion_id"
     t.integer "user_id"
+    t.string "slug"
   end
 
   create_table "users", force: :cascade do |t|
