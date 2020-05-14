3a38dea3a5ac6f09d3cdc3b1ecbef3ddccc41c48
javitocor <javitocor@gmail.com>
2020-05-14 12:59:36 +0200

    Added views to channels

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :discussions, dependent: :destroy 
+  has_many :channels, through: :discussions 
 end
